cef03318e8d53a3e94f712a9de199816208a0e2c
ccoyle <ccoyle@gmail.com>
2021-09-01 16:59:26 -0400

    Initial commit.

---
+++ main.py
@@ -0,0 +1,51 @@
+#!env/bin/python
+
+from time import sleep
+
+import asyncio
+import aiohttp
+
+from aioeagle import EagleHub
+from prometheus_client import start_http_server, Summary, Gauge, Enum
+
+import config
+
+REQUEST_TIME = Summary(
+    'request_processing_seconds',
+    'Time spent processing request'
+)
+connected = Enum(
+    'meter_connected',
+    'Connection state to meter',
+    states=['connected', 'disconnected']
+)
+
+demand = Gauge('instantaneous_demand', 'Instantaneous Demand')
+consumed = Gauge('total_consumed', 'Current Consumed KWh to date')
+
+@REQUEST_TIME.time()
+async def process_request():
+    async with aiohttp.ClientSession() as session:
+        await run(session)
+
+async def run(session):
+    hub = EagleHub(
+        session,
+        config.CLOUD_ID,
+        config.INSTALL_CODE,
+        host=config.EAGLE_IP
+    )
+
+    devices = await hub.get_device_list()
+    metrics = await devices[0].get_device_query()
+
+    connected.state('connected' if devices[0].is_connected else 'disconnected')
+    demand.set(metrics['zigbee:InstantaneousDemand']['Value'])
+    consumed.set(metrics['zigbee:CurrentSummationDelivered']['Value'])
+
+
+if __name__ == '__main__':
+    start_http_server(8888)
+    while True:
+        asyncio.run(process_request())
+        sleep(30)
